cc315a1a989acc45f54fb032e6a45aa531e18095
Dalang Ly <49807152+DalangLy@users.noreply.github.com>
2022-01-08 15:43:10 +0700

    setup route

---
+++ lib/config/routes/router.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:project_structure/features/home/presentation/pages/home_wrapper_page.dart';
+import 'package:project_structure/features/login/presentation/pages/login_page.dart';
+
+@MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    AutoRoute(page: HomeWrapperPage, initial: true, path: '/',),
+    AutoRoute(page: LoginPage, path: '/login'),
+  ],
+)
+class $AppRouter {}
